2d76c863729c6dc03dee74eda6eedda6fec36978
xuyuetong <xuyuetong2025@163.com>
2026-03-06 11:05:27 +0800

    chapter1: hello kernel/name

--- code/os/01-helloRVOS/kernel.c
+++ code/os/01-helloRVOS/kernel.c
@@ -5,7 +5,10 @@ void start_kernel(void)
 {
 	uart_init();
 	uart_puts("Hello, RVOS!\n");
+	uart_puts("Hello, Kernel!\n");
+	uart_puts("Hello, Xu Yuetong!\n");
 
-	while (1) {}; // stop here!
+	while (1)
+	{
+	}; // stop here!
 }
-
